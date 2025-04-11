a7231b70bad5935fa7eacce7b7918907136b38bd
Prahlad Yeri <prahladyeri@yahoo.com>
2025-04-11 12:24:24 +0530

    chore: aligned controls

--- focusbeam/focusbeam/MainForm.Designer.cs
+++ focusbeam/focusbeam/MainForm.Designer.cs
@@ -30,13 +30,14 @@ namespace focusbeam
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnDashboard = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.lblTracker = new System.Windows.Forms.Label();
             this.button5 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,9 +49,10 @@ namespace focusbeam
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.label1, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnSettings, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnStart, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblTracker, 4, 1);
             this.tableLayoutPanel1.Controls.Add(this.button5, 4, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.button3, 3, 0);
             this.tableLayoutPanel1.Controls.Add(this.button2, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
@@ -65,88 +67,102 @@ namespace focusbeam
             this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 66);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // btnDashboard
+            // lblTracker
             // 
-            this.btnDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnDashboard.Location = new System.Drawing.Point(4, 3);
-            this.btnDashboard.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Size = new System.Drawing.Size(148, 27);
-            this.btnDashboard.TabIndex = 0;
-            this.btnDashboard.Text = "🚀 Dashboard";
-            this.btnDashboard.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(160, 3);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 27);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "💬 Notes";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(316, 3);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 27);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "🧠 Mind Maps";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button3.Location = new System.Drawing.Point(472, 3);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(148, 27);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "💡 MCQ";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Location = new System.Drawing.Point(4, 36);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(148, 27);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "▶️ Start";
-            this.button4.UseVisualStyleBackColor = true;
+            this.lblTracker.AutoSize = true;
+            this.lblTracker.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTracker.Location = new System.Drawing.Point(624, 33);
+            this.lblTracker.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTracker.Name = "lblTracker";
+            this.lblTracker.Size = new System.Drawing.Size(160, 33);
+            this.lblTracker.TabIndex = 11;
+            this.lblTracker.Text = "🕒 00:00:00";
+            this.lblTracker.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button5
             // 
             this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button5.Location = new System.Drawing.Point(628, 3);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button5.Location = new System.Drawing.Point(624, 0);
+            this.button5.Margin = new System.Windows.Forms.Padding(0);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(152, 27);
+            this.button5.Size = new System.Drawing.Size(160, 33);
             this.button5.TabIndex = 4;
             this.button5.Text = "❓ About";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // button3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(627, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 33);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "🕒 00:00:00";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Location = new System.Drawing.Point(468, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "💡 MCQ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(312, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "🧠 Mind Maps";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(156, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "💬 Notes";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 0);
+            this.btnDashboard.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(156, 33);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "🚀 Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.btnStart, 3);
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnStart.Location = new System.Drawing.Point(156, 33);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(0);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(468, 33);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "▶️ Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSettings.Location = new System.Drawing.Point(0, 33);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(156, 33);
+            this.btnSettings.TabIndex = 5;
+            this.btnSettings.Text = "⚙️ Settings";
+            this.btnSettings.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -174,9 +190,10 @@ namespace focusbeam
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnDashboard;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTracker;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSettings;
     }
 }
 
